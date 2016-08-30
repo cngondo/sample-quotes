bcb1c94f220782170c942616444e6c5634ba8407
cngondo <devngondo@gmail.com>
2016-08-30 13:38:05 +0300

    splash activity

--- app/src/main/java/introslider/example/com/introslider/SplashActivity.java
+++ app/src/main/java/introslider/example/com/introslider/SplashActivity.java
@@ -1,13 +1,30 @@
 package introslider.example.com.introslider;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_INTERVAL = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        Request fullscreen activity
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
+            }
+        }, SPLASH_INTERVAL);
     }
 }
